5055ea53f99f2e4efdd23f31b2851f3234dfa865
Nguyễn Trần Minh Tâm <147505363+taamnguyeen04@users.noreply.github.com>
2025-03-26 10:52:46 +0700

    server

--- server.py
+++ server.py
@@ -1,21 +1,67 @@
+import socket
+import threading
 import cv2
 
-RTSP_URL = "rtsp://192.168.0.103:8554/stream"
+TCP_IP = "0.0.0.0"
+TCP_PORT = 5005
 
-cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
-cap.set(cv2.CAP_PROP_BUFFERSIZE, 2)
-cap.set(cv2.CAP_PROP_FPS, 15)
+UDP_URL = "udp://192.168.0.103:1234"
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        print("Alo")
-        break
 
-    cv2.imshow("RTSP Stream", frame)
+def receive_tcp_data():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((TCP_IP, TCP_PORT))
+    server_socket.listen(1)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+    print(f"Listen IP/PORT {TCP_IP}:{TCP_PORT}...")
 
-cap.release()
-cv2.destroyAllWindows()
+    conn, addr = server_socket.accept()
+    print(f"addr {addr}")
+
+    while True:
+        try:
+            data = conn.recv(1024).decode().strip()
+            if not data:
+                break
+            print(f"data: {data}")
+        except Exception as e:
+            print("Lỗi:", e)
+            break
+
+    conn.close()
+    server_socket.close()
+    print("đóng kết nối TCP.")
+
+
+def stream_video():
+    cap = cv2.VideoCapture(UDP_URL, cv2.CAP_FFMPEG)
+
+    if not cap.isOpened():
+        print("Không video!")
+        return
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Không frame!")
+            break
+
+        cv2.imshow("Stream Video", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+thread_tcp = threading.Thread(target=receive_tcp_data)
+thread_udp = threading.Thread(target=stream_video)
+
+thread_tcp.start()
+thread_udp.start()
+
+thread_tcp.join()
+thread_udp.join()
+
+
